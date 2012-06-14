e317e92fc276442e945ef4cbc9bed5fe223f6031
Negoro Kazuki <s63.kazuki.n@gmail.com>
2012-06-14 21:28:21 +0900

    scaffoldを作成しました。 * users * books * book_users

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialLibrary::Application.config.secret_token = 'f961f852c0a19132031d3c0229bc8d340d7cbab1de16e95e45f86d032e750f878fec590a45d6ee8cb6a816a056105f03a9e48f51ebfba7e902ff983f22ad2593'
